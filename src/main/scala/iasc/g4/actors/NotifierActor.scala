3fd02cc4eab9a54c483076c1920d3b78db8e96de
Agus Nahas <m.agustina.nahas@gmail.com>
2020-07-16 01:40:55 -0300

    Ya tenemos pool de actores, no tengo idea de como funciona

--- src/main/scala/iasc/g4/actors/NotifierActor.scala
+++ src/main/scala/iasc/g4/actors/NotifierActor.scala
@@ -1,17 +1,60 @@
 package iasc.g4.actors
 import iasc.g4.models.Models.{Command, OperationPerformed}
 
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.Future
+import scala.util.{ Failure, Success }
 
 /**
  * Actor que se encarga de enviar un mensaje de notificación a una lista de destinatarios
  */
-class NotifierActor {
-//  final case class Notification(whom: String, replyTo: ActorRef[Notified])
-//  final case class Notified(whom: String, owner: ActorRef[Notification])
-//
-//  def apply(): Behavior[Notification] = Behaviors.receive { (ctx, message) =>
-//    ctx.log.info("New auction that may interest you!", message.whom)
-//    message.replyTo ! Notified(message.whom, ctx.self)
-//    Behaviors.same
-//  }
+object NotifierActor {
+  final case class Notification(buyers: String, replyTo: ActorRef[String])  extends Command
+
+  def apply(): Behavior[Command] = Behaviors.setup { context =>
+
+    Behaviors.receiveMessage {
+      case Notification(buyers, replyTo) =>
+        context.log.info("***********************************************************************************************************************************************Got message {}", buyers)
+
+        implicit val system = ActorSystem()
+        implicit val materializer = ActorMaterializer()
+        implicit val executionContext = system.dispatcher
+
+        val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8081/buyers"))
+        responseFuture
+          .onComplete {
+            case Success(res) => OperationPerformed("TBD")
+            case Failure(_)   => sys.error("----------------------------------------------------------------------------------------------------------------something wrong")
+          }
+        Behaviors.same
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/scala/iasc/g4/actors/NotifierSpawnerActor.scala
+++ src/main/scala/iasc/g4/actors/NotifierSpawnerActor.scala
@@ -1,26 +1,15 @@
 package iasc.g4.actors
 
-import akka.actor.typed.Behavior
 import iasc.g4.models.Models.Command
 import iasc.g4.actors.NotifierActor
 import iasc.g4.models.Models.OperationPerformed
+import iasc.g4.models.Models.Buyers
 
-
-import iasc.g4.models.Models.{Buyers}
-
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.{ActorRef, Behavior}
-
-
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.stream.ActorMaterializer
-
+import akka.actor.typed.scaladsl.{ Behaviors, Routers }
 import scala.concurrent.Future
-import scala.util.{ Failure, Success }
-
+import scala.util.{Failure, Success}
+import akka.actor.typed.{ Behavior, SupervisorStrategy }
+import akka.actor.typed.{ActorRef, Behavior}
 
 /**
  * Encargado de asignar a actores Notifier la tarea de enviar una notificación, previamente obeniendo la lista de
@@ -31,68 +20,59 @@ object NotifierSpawnerActor {
   final case class NotifyBuyers(buyersNotified: Buyers, replyTo: ActorRef[String]) extends Command
 //
 
+//  def apply(): Behavior[Command] =
+//    Behaviors.receiveMessage {
+//      case NotifyBuyers(buyersNotified, replyTo) =>
+//        implicit val system = ActorSystem()
+//        implicit val materializer = ActorMaterializer()
+//        // needed for the future flatMap/onComplete in the end
+//        implicit val executionContext = system.dispatcher
+//
+//        val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8081/buyers"))
+//
+//        responseFuture
+//          .onComplete {
+//            case Success(res) => replyTo ! "Notified!"
+//            case Failure(_)   => sys.error("something wrong")
+//          }
+//        Behaviors.same
+//    }
+
+
   def apply(): Behavior[Command] =
-    Behaviors.receiveMessage {
-      case NotifyBuyers(buyersNotified, replyTo) =>
-        implicit val system = ActorSystem()
-        implicit val materializer = ActorMaterializer()
-        // needed for the future flatMap/onComplete in the end
-        implicit val executionContext = system.dispatcher
+    Behaviors.setup { ctx =>
 
-        val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "http://localhost:8081/buyers"))
+      val pool = Routers.pool(poolSize = 5)(
+        // make sure the workers are restarted if they fail
+        Behaviors.supervise(NotifierActor()).onFailure[Exception](SupervisorStrategy.restart))
+      val router = ctx.spawn(pool, "notifier-pool")
 
-        responseFuture
-          .onComplete {
-            case Success(res) => replyTo ! "Notified!"
-            case Failure(_)   => sys.error("something wrong")
+      Behaviors.receiveMessage {
+        case NotifyBuyers(buyersNotified, replyTo) =>
+
+          (0 to 5).foreach { n =>
+            router ! NotifierActor.Notification(buyersNotified.buyers.size.toString, replyTo)
           }
 
-
-
-        Behaviors.same
-    }
-
-  def apply(): Behavior[Command] =
-    Behaviors.setup { context =>
-      //#create-actors
-      val notifier = context.spawn(NotifierActor(), "Notifier") //notifier
-      //#create-actors
-
-      Behaviors.notifyBuyers { newAuction, buyers =>
-
-        // Como le notifico a ESE buyer interesado ?
-
-        ctx.log.warn("Coso coso")
-        Behaviors.same
-//
-//        val replyTo = context.spawn(NotifierBot(max = 3), newAuction.title)
-//        notifier ! NotifierActor.Notification(newAuction.title, buyer)
-//        Behaviors.same
+          replyTo ! "Listo!"
+          Behaviors.same
       }
     }
 
 }
 
-// newAuction.title
-// newAuction.owner
-// newAuction.whom
 
-//object NotifierBot {
-//
-//  def apply(max: Int): Behavior[NotifierActor.Notified] = {
-//    bot(0, max)
-//  }
-//
-//  private def bot(notificationingCounter: Int, max: Int): Behavior[NotifierActor.Notified] =
-//    Behaviors.receive { (context, newAuction) =>
-//      val n = notificationingCounter + 1
-//      context.log.info("Notification {} for {}", n, newAuction.whom)
-//      if (n == max) {
-//        Behaviors.stopped
-//      } else {
-//        newAuction.owner ! NotifierActor.Notification(newAuction.whom, context.self)
-//        bot(n, max)
-//      }
-//    }
-//}
-//#Notifier-bot
+
+
+
+
+
+
+
+
+
+
+
+
+
+
