2bb741978ed014c4c8308421a51da1e527846faf
sattjoa <jsatta@bancoindustrial.com.ar>
2020-07-21 00:22:52 -0300

    Casos 1 a 5 sin notificaciones

--- cliente/src/main/java/utn/frba/iasc/controller/ClienteController.java
+++ cliente/src/main/java/utn/frba/iasc/controller/ClienteController.java
@@ -10,13 +10,61 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
-
-	@GetMapping("/{message}")	
-	public ResponseEntity<String> recibirNotificacion(@PathParam("id") String id, @PathParam("message") String message) {
-		String resp = "Mensaje: " + message + "; Subasta: "+ id;
-		System.out.println(resp);
-		HttpHeaders respHeaders = new HttpHeaders();
-		respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-		return new ResponseEntity<>("Mensaje: " + resp + "; Subasta: "+ id, respHeaders, HttpStatus.OK);
+	
+	@GetMapping("/subastaGanada")	
+	ResponseEntity<?> subastaGanada (
+			@PathParam("id") String id
+			) throws Exception{
+				String responseSt = "Gané la subasta "+id+" :)";
+				System.out.println(responseSt);
+				HttpHeaders respHeaders = new HttpHeaders();
+				respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+				return new ResponseEntity<String>(responseSt, respHeaders, HttpStatus.OK);
 	}
+	
+	@GetMapping("/nuevaSubasta")	
+	ResponseEntity<?> nuevaSubasta (
+			@PathParam("id") String id
+			) throws Exception{
+				String responseSt = "Me invitaron a la subasta "+id+" :)";
+				System.out.println(responseSt);
+				HttpHeaders respHeaders = new HttpHeaders();
+				respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+				return new ResponseEntity<String>(responseSt, respHeaders, HttpStatus.OK);
+	}
+
+	@GetMapping("/subastaPerdida")	
+	ResponseEntity<?> subastaPerdida (
+			@PathParam("id") String id
+			) throws Exception{
+				String responseSt = "Perdí la la subasta "+id+" :(";
+				System.out.println(responseSt);
+				HttpHeaders respHeaders = new HttpHeaders();
+				respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+				return new ResponseEntity<String>(responseSt, respHeaders, HttpStatus.OK);
+	}
+	
+	@GetMapping("/nuevoPrecio")	
+	ResponseEntity<?> subastaPerdida (
+			@PathParam("id") String id,
+			@PathParam("precio") Double precio
+			) throws Exception{
+				String responseSt = "El nuevo precio de la subasta "+id+" es "+String.valueOf(precio);
+				System.out.println(responseSt);
+				HttpHeaders respHeaders = new HttpHeaders();
+				respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+				return new ResponseEntity<String>(responseSt, respHeaders, HttpStatus.OK);
+	}
+	
+	@GetMapping("/subastaCancelada")	
+	ResponseEntity<?> subastaCancelada (
+			@PathParam("id") String id
+			) throws Exception{
+				String responseSt = "La subasta "+id+" fue cancelada ";
+				System.out.println(responseSt);
+				HttpHeaders respHeaders = new HttpHeaders();
+				respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+				return new ResponseEntity<String>(responseSt, respHeaders, HttpStatus.OK);
+	}
+
 }
